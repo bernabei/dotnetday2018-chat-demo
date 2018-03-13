89035af1d56b3b3b3ab33c8d1b08030f3643965e
Fabrizio Bernabei <bernabei@gmail.com>
2018-03-13 23:55:16 +0100

    initial project

---
+++ web/Pages/Account/Lockout.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dnu_chat_demo.Pages.Account
+{
+    public class LockoutModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
